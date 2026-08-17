18b5b5a554b8fa5d01e12b4a05669bc88d9c0238
shivamsharma05080 <shivamsharma05080@gmail.com>
2026-08-17 22:36:33 +0530

    Add initial build.gradle.kts configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.spicy.launcher"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.spicy.launcher"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
